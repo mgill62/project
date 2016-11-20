5bc922b8a8547c7187127a00dc9f7b544fe54d99
Sahil Jassal <sahil.jassal@team.telstra.com>
2016-11-20 23:07:34 +0530

    Integrating EDIT/UPDATE Profile by Riya

---
+++ Create Forum Project/Create Forum Project/Profile.aspx
@@ -0,0 +1,128 @@
+﻿<%@ Page Title="Your Profile - Forum" Language="C#" AutoEventWireup="true" CodeBehind="Profile.aspx.cs" Inherits="Create_Forum_Project.Profile" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="CSS/dashboard.css" rel="stylesheet" type="text/css" />
+    <link href="CSS/divlogo.css" rel="stylesheet" type="text/css" />
+    <style type="text/css">
+
+        .auto-style1 {
+            width: 100%;
+            border-style: solid;
+            border-width: 1px;
+        }
+        .auto-style3 {
+            width: 136px;
+        }
+        .auto-style4 {
+            height: 47px;
+        }
+        .auto-style5 {
+            border-collapse: collapse;
+            border-style: none;
+            border-color: inherit;
+            border-width: 0;
+            margin-top: 0.75em;
+        }
+        .auto-style6 {
+            border-style: none;
+            border-color: inherit;
+            border-width: 0;
+            padding-left: 0em;
+            padding-right: 2em;
+            padding-top: 0.25em;
+            padding-bottom: 0.25em;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="divlogo">Ask</div>
+    <div class="div_header">
+     Welcome : <label id="lblwelcom" runat="server"/>&nbsp;&nbsp;&nbsp;  <asp:Button ID ="btnProfile" runat="server" OnClick="btnProfile_Click" Text="Your Profile"  /> 
+    </div>
+    <div class="div_logout"><a href="forumLogin.aspx">Logout</a></div>
+      <div id="div_dashboard_box" class="div_dashboard" runat="server" visible="false">
+     <table>
+      <tr>
+       <th>Dashboard</th>
+      </tr>
+      <tr>
+       <td><a href="Default.aspx">Home Page</a></td>
+      </tr>
+      <tr>
+       <td><a href="Dashboard.aspx">Post Articles</a></td>
+      </tr>
+      <tr>
+       <td><a href="Home.aspx">Your Articles</a></td>
+      </tr>
+     </table>
+    </div>
+    <div id="div_table">
+        <table class="auto-style5" style="border-spacing: 0;">
+            <tr>
+                <td class="auto-style6">&nbsp;
+                    <asp:Label ID="lblPic" runat="server" Text="Profile Picture"></asp:Label>
+                </td>
+                <td class="auto-style6">
+                    <asp:Image ID="imgProfile" runat="server" Height="150" Width="150" />
+                </td>
+                <td class="auto-style6">
+                    <asp:FileUpload ID="FileUpload1" runat="server" />
+                    &nbsp;
+                    <asp:Button ID="btnUpload" runat="server" OnClick="btnUpload_Click" Text="Upload" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style6" colspan="3">&nbsp;
+                    <asp:TextBox ID="txtFirstName" runat="server" MaxLength="50" placeholder="First Name"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style6" colspan="3">&nbsp;
+                    <asp:TextBox ID="txtLastName" runat="server" MaxLength="50" placeholder="Last Name"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style6" colspan="3">&nbsp;
+                    <asp:TextBox ID="txtMiddleName" runat="server" MaxLength="50" placeholder="Middle Name"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style6" colspan="3">&nbsp;
+                    <asp:TextBox ID="txtContactNo" runat="server" MaxLength="15" placeholder="Contact Number" ToolTip="With country code"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style6" colspan="3">&nbsp;
+                    <asp:TextBox ID="txtCollege" runat="server" MaxLength="50" placeholder="College"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style6" colspan="3">&nbsp;
+                    <asp:TextBox ID="txtProfession" runat="server" MaxLength="50" placeholder="Profession"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style6" colspan="3">&nbsp;
+                    <asp:TextBox ID="txtLocation" runat="server" MaxLength="50" placeholder="Location"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style6" colspan="3">&nbsp;
+                    <asp:TextBox ID="txtDescription" runat="server" Height="30px" MaxLength="500" placeholder="Description about you" Width="450px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style6" colspan="3">&nbsp;
+                    <asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Update" />
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Create Forum Project/Create Forum Project/Profile.aspx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+namespace Create_Forum_Project
+{
+    public partial class Profile : System.Web.UI.Page
+    {
+        string user1;
+        int ID;
+        string datetime = System.DateTime.Now.Date.ToLongDateString();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["nam"] == null)
+                {
+                    Response.Redirect("~/forumLogin.aspx");
+                }
+                else
+                {
+                    user1 = Session["nam"].ToString();
+                   // ID = Convert.ToInt32(Session["ID"].ToString());
+                    lblwelcom.InnerHtml = user1.ToString();
+                    div_dashboard_box.Visible = true;
+
+                    if (Int32.TryParse(Request.QueryString["id"], out ID))
+                    {
+                        DataTable profile = getProfile(ID);
+                        DisplayProfile(profile);
+                        imgProfile.ImageUrl = "~/images/" + ID + ".jpg";
+                    }
+                }
+            }
+        }
+
+         //Getting data from View Stored Proc
+        DataTable getProfile(Int32 profileId)
+        {
+            DataTable pT = new DataTable();
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["FRCon"].ConnectionString;
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "usp_ViewProfile";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Pid", profileId);
+            cmd.Connection = con;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(pT);
+            return pT;
+        }
+
+        //Adding values from fetched data to text boxes
+        void DisplayProfile(DataTable dt)
+        {
+            if(dt.Rows.Count > 0)
+            {
+                txtFirstName.Text = dt.Rows[0].GetStringValue("FirstName");
+                txtLastName.Text = dt.Rows[0].GetStringValue("LastName");
+                txtMiddleName.Text = dt.Rows[0].GetStringValue("MiddleName");
+                txtContactNo.Text = dt.Rows[0].GetStringValue("ContactNo");
+                txtCollege.Text = dt.Rows[0].GetStringValue("College");
+                txtLocation.Text = dt.Rows[0].GetStringValue("Location");
+                txtDescription.Text = dt.Rows[0].GetStringValue("Description");
+                txtProfession.Text = dt.Rows[0].GetStringValue("Profession");
+            }            
+        }
+
+       //On clicking update button
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Int32 id, ret = 0;
+            Int32.TryParse(Request.QueryString["id"], out id);
+            
+            //Executing Update Stored Proc
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["FRCon"].ConnectionString;
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "usp_UpdateProfile";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Pid", id);
+            cmd.Parameters.AddWithValue("@Pfirstname", txtFirstName.Text.ToString());
+            cmd.Parameters.AddWithValue("@Plastname", txtLastName.Text.ToString());
+            cmd.Parameters.AddWithValue("@Pmiddlename", txtMiddleName.Text.ToString());
+            cmd.Parameters.AddWithValue("@Pcontact", txtContactNo.Text.ToString());
+            cmd.Parameters.AddWithValue("@Plocation", txtLocation.Text.ToString());
+            cmd.Parameters.AddWithValue("@Pdesc",txtDescription.Text.ToString());
+            cmd.Parameters.AddWithValue("@Pcollege", txtCollege.Text.ToString());
+            cmd.Parameters.AddWithValue("@Pprofession",txtProfession.Text.ToString());
+
+            //To capture output from Stored Procedure
+            SqlParameter retval = new SqlParameter("@Pret", DbType.Int32);
+            retval.Direction = ParameterDirection.Output;
+            cmd.Parameters.Add(retval);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            ret = Convert.ToInt32(retval.Value);
+            con.Close();
+
+            if (ret == 0)
+                Response.Write("<script>alert('Error while updating profile')</script");
+            else if (ret == 1)
+                Response.Write("<script>alert('Profile updated successfully')</script");            
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            Int32 id = 0;
+            Int32.TryParse(Request.QueryString["id"], out id);
+            if (FileUpload1.HasFile)
+            {
+                string fileName = id.ToString();
+                FileUpload1.PostedFile.SaveAs(Server.MapPath("~/images/") + fileName + ".jpg");
+                Response.Redirect(Request.Url.AbsoluteUri);
+            }
+        }
+        protected void btnProfile_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Profile.aspx?id=" + ID);
+        }
+
+    }
+
+    //Class to get value of column
+    public static class Extensions
+    {
+        public static string GetStringValue(this DataRow dr, string columnName)
+        {
+            string res = string.Empty;
+            if (dr.Table.Columns.Contains(columnName))
+            {
+                //If value of that column is null then return empty string nor return the value
+                res = Convert.IsDBNull(dr[columnName]) ? string.Empty : dr[columnName].ToString();
+            }
+            return res;
+        }
+    }
+}
